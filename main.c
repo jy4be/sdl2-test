a4bec3350cd40816decf0cf3c7dccd213519f247
jy4be <derlegoman@web.de>
2022-03-04 01:12:50 +0100

    First SDL Integrations

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdint.h>
 
-int main(){
+void initSDL();
+
+int main(int argc, char **argv){
+
 	printf("Hello world!\n");
 	return 0;
+}
+
+void initSDL(){
+	SDL_Init(SDL_INIT_VIDEO);
 }
